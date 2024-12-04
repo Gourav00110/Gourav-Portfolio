6d0627deaf04de94f462bcc1502901d884cc9e97
Gourav00110 <163579777+Gourav00110@users.noreply.github.com>
2024-12-04 09:58:46 +0500

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'gouravmandloi@gmail.com',
+    email: 'mandloigourav878@gmail.com',
     phone: '+919009619767',
     address: 'Near Khajrana Scheme 128,Indore ',
 
